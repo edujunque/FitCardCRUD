74fd0787d2fcc950fd4cfb022bf13b7d4d211e1c
Eduardo Junque <eduardo.junque@navegarti.com.br>
2017-03-25 11:25:33 -0300

    Inclusão das models

---
+++ FitCardCRUD/FitCardCRUD/Models/Estabelecimento.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FitCardCRUD.Models
+{
+    public class Estabelecimento
+    {
+        public Guid idEstabelecimento { get; set; }
+        public string estabNome { get; set; }
+        [Required]
+        public string estabNomeFantasia { get; set; }
+        [Required]
+        public string estabCNPJ { get; set; }
+        public string estabEmail { get; set; }
+        public string estabTelefone { get; set; }
+        public bool estabStatus { get; set; }
+        public Categoria categoria { get; set; }
+
+        //Categoria[Supermercado, Restaurante, Borracharia, Posto, Oficina];
+    }
+}
